9b612f937134aad876c187266c43cbf95309cda0
selva suryaaa <selva97eee@gmail.com>
2023-07-26 22:03:40 +0530

    App Finished

--- src/Pages/CRUD.js
+++ src/Pages/CRUD.js
@@ -1,58 +1,98 @@
 import React, { useEffect, useState } from "react"
 import axios from 'axios';
 import "./Crud.css";
-import { ToastContainer, toast } from 'react-toastify';
+import { Bounce, Flip, ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Table, TableCell, TableHeaderCell, TableRow, TableHeader, TableBody } from "semantic-ui-react";
+
+// "https://64bd1df32320b36433c76e5a.mockapi.io/accounts"
 
 const CRUD = () => {
-    // const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
     const [users, setUsers] = useState([])
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [editId, setEditId] = useState(null)
+    const [show, setShow] = useState(false)
+    const [showSubmit, setShowSubmit] = useState(true)
+
+
+
+
     // const [refresh, setRefresh] = useState(false)
 
     useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/users")
-            .then(res => {
-                setUsers(res.data)
-                // setLoading(prev => !prev)
-                setEditId(res.data.id)
-                // toast('Data Retrived')
-            })
-            .catch(err => console.log('ERROR GET', err));
+        getAllUsers();
     }, [])
+    const getAllUsers = async () => {
+        try {
+            let res = await axios.get("https://64bd1df32320b36433c76e5a.mockapi.io/accounts")
+            setUsers(res.data)
+            setLoading(false)
+            // setEditId(res.data.id)
+            // toast.info('Data Retrived')
+        }
+        catch (err) {
+            console.log('ERROR GET', err);
+        }
+    }
+    const validate = () => {
 
+        if (name == "") {
+            toast.error('Name reqired')
+            return
+        }
+        if (email == '') {
+            toast.error('Email reqired')
+            return
+        }
+        else {
+            let emailcheck = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+            if (!emailcheck.test(email)) {
+                toast.error('Not a valid Email')
+                return
+            }
+        }
+        return true 
+    }
 
     const addHandler = async (e) => {
         e.preventDefault();
+        let isValid = validate()
+        if(isValid)
+
+        // if (isValid == false){
+        // toast.error("Invalid Form")
+        // return
+
         try {
-            let response = await axios.post(`https://jsonplaceholder.typicode.com/users`, {
+            let response = await axios.post(`https://64bd1df32320b36433c76e5a.mockapi.io/accounts/`, {
                 // id,
                 name: name,
                 email: email,
             })
             if (response.status == 201) {
                 console.log('POST RESPONSE', response.data)
-                setUsers(prev => [response.data, ...prev])
+                setUsers(prev => [...prev, response.data])
                 setName('')
                 setEmail('')
-                // toast(`${response.data['name']} added`)
+                toast.success(`${response.data['name']} added`)
             }
         }
         catch (err) {
             console.log('ERROR', err)
         }
+
     }
     const deleteHandler = async (id) => {
         try {
             let selected = users.find(item => item.id == id)
             console.log('SELECTED ITEM TO DELETE', selected)
-            let response = await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+            let response = await axios.delete(`https://64bd1df32320b36433c76e5a.mockapi.io/accounts/${id}`)
             if (response.status == 200) {
                 console.log('RESPONSE', response.data)
                 setUsers(users.filter(user => user.id !== id))
-                toast(`${selected.name} Deleted`)
+                toast.success(`${selected.name} Deleted`)
             }
         }
         catch (err) {
@@ -60,35 +100,63 @@ const CRUD = () => {
         }
     }
     const editHandler = (id) => {
-        axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+        setShow(true)
+        setShowSubmit(false)
+        axios.get(`https://64bd1df32320b36433c76e5a.mockapi.io/accounts/${id}`)
             .then(res => {
-                setName(users[id - 1].name)
-                setEmail(users[id - 1].email)
-                setEditId(users[id - 1].id)
-                console.log(users[id - 1])
+                if (res) {
+                    setName(res.data.name)
+                    setEmail(res.data.email)
+                    setEditId(id)
+                    toast.info('Single User Fetched')
+
+                    // setEditId(users[id - 1].id)
+                    // setName(users[id - 1].name)
+                    // setEmail(users[id - 1].email)
+                    // console.log(users[id - 1])
+                }
             })
             .catch(err => console.log(err))
     }
 
     const handleDataUpdate = () => {
+        let selected = users.find(item => item.id == editId)
+        console.log('SELECTED ITEM TO Edit', selected)
+        if (name == "") {
+            toast.error('Name reqired')
+            return
+        }
+        if (email == '') {
+            toast.error('Email reqired')
+            return
+        }
+        else {
+            let emailcheck = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+            if (!emailcheck.test(email)) {
+                toast.error('Not a valid Email')
+                return
+            }
+        }
+        
 
-        axios.put(`https://jsonplaceholder.typicode.com/users/${editId}`, {
+        axios.put(`https://64bd1df32320b36433c76e5a.mockapi.io/accounts/${editId}`, {
             name: name,
             email: email,
-            // editId: editId
         })
             .then((res) => {
-
-                if (res.status == 200) {
+                if (res.status == 200)
+                    // return
                     console.log('put req res', res.data);
-                    console.log(name, email)
-                    setUsers(prev => [res.data, ...prev])
-                    setName('')
-                    setEmail('')
-                }
+                toast.success(`${selected.name} Updated to ${res.data.name}`)
+                getAllUsers();
+                setName('')
+                setEmail('')
+                setShow(false)
             })
             .catch(err => console.log('put req error', err))
     }
+
+
     // setUsers([...users.filter(element => element.id == editId-1 ? res.data : element)])
     //     // setUsers(res.data)
     //     //     setUsers(
@@ -100,16 +168,23 @@ const CRUD = () => {
 
     // }
 
-    // if (loading) {
-    //     return (
-    //         <div style={{ height: '100vh', color: 'brown', verticalAlign: 'center', fontWeight: 'bold', fontSize: '30px', display: 'grid', placeContent: 'center' }}>Loading...</div>)
-    // }
+    if (loading) {
+        return (
+            <div style={{ height: '100vh', color: 'brown', verticalAlign: 'center', fontWeight: 'bold', fontSize: '30px', display: 'grid', placeContent: 'center' }}>Loading...</div>)
+    }
+
+
     return (
         <div className="crud">
-
-            <ToastContainer />
-            {/* <span >{loading}</span> */}
-
+            <span >{loading}</span>
+            <ToastContainer
+                // autoClose
+                delay={2000}
+                pauseOnHover={false}
+                // draggablePercent={50}
+                transition={Flip}
+                theme="dark"
+            />
             <div>
                 <div>
                     <h2>Add User</h2>
@@ -126,39 +201,84 @@ const CRUD = () => {
                         placeholder="Enter Email"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)} />
-                    <button onClick={addHandler}>Submit</button>
-                    <button onClick={handleDataUpdate} >Update</button>
+                    {showSubmit ? <button onClick={addHandler}>Submit</button> : null}
+                    {show ?
+                        <button style={{ backgroundColor: 'yellow', color: 'black' }}
+                            onClick={handleDataUpdate} >Update</button>
+                        : null}
 
                 </div>
             </div>
 
-            {users !== [] ?
-                users.map((user, index) =>
-                    <ul className="list" key={index} >
-                        <li className="list-item" >
-                            <span>
-                                {user.name}
-                            </span>
-                            {user.email}
+            <Table className="ui celled green large" padded={false} collapsing={true} color='orange' sortable textAlign="center" style={{ margin: '20px' }}>
+                <TableHeader>
+                    <TableRow>
+                        <TableHeaderCell sorted='descending'>ID</TableHeaderCell>
+                        <TableHeaderCell sorted='descending'>Name</TableHeaderCell>
+                        <TableHeaderCell sorted='descending'>Email</TableHeaderCell>
+                        <TableHeaderCell sorted='descending'>Actions</TableHeaderCell>
 
-                        </li>
-                        <div className="btn-grp">
-                            <button onClick={() => {
-                                editHandler(user.id)
-                                // setEditId(user.id)
-                            }
-                            }>Edit</button>
-                            <button
-                                onClick={() => deleteHandler(user.id)}>
-                                Delete
-                            </button>
-                        </div>
-                    </ul>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {users !== [] ?
+                        users.map((user, index) => {
+                            return (
+                                <TableRow key={user.id}>
+                                    <Table.Cell width={1}>{user.id} </Table.Cell>
+                                    <Table.Cell width={2}>{user.name}</Table.Cell>
+                                    <Table.Cell width={2}>{user.email}</Table.Cell>
+                                    <Table.Cell width={2}>
+                                        <button onClick={() => {
+                                            editHandler(user.id)
+                                            setEditId(user.id)
+                                        }
+                                        }>Edit</button>
+                                        <button style={{ backgroundColor: 'red', color: 'white' }}
+                                            onClick={() => deleteHandler(user.id)}>
+                                            Delete
+                                        </button>
+                                    </Table.Cell>
 
-                ) : null
-            }
+                                </TableRow>
+                            )
+                        }) : null}
+                </TableBody>
+            </Table>
+
         </div>
     )
-};
+}
+
+
+
+{/* <ul className="list" key={index} >
+                <li className="list-item" >
+                    <span>
+                        {user.name}
+                    </span>
+                    {user.email}
+
+                </li>
+                <div className="btn-grp">
+                    <button onClick={() => {
+                        editHandler(user.id)
+                        setEditId(user.id)
+                    }
+                    }>Edit</button>
+                    <button style={{ backgroundColor: 'red', color: 'white' }}
+                        onClick={() => deleteHandler(user.id)}>
+                        Delete
+                    </button>
+                </div>
+            </ul> */}
+
+{/* )
+            : null */}
+
+//         </div>
+//     )
+// }
+
 
 export default CRUD;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'semantic-ui-css/semantic.min.css' 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import CRUD from './Pages/CRUD';
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
     <CRUD/>
+    // <App/>
   // </React.StrictMode>
 );
 
